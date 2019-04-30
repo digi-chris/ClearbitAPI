4cf6aba1b04e8ac59203ee5e0798616ded6137fa
digi-chris <chris.barnard@btinternet.com>
2019-04-30 10:05:06 +0100

    Clearbit Logo API needs Accept-Encoding header

--- ClearbitAPI/Logo.cs
+++ ClearbitAPI/Logo.cs
@@ -12,6 +12,7 @@ namespace BarnardTechnology.ClearbitAPI
         public static Bitmap GetCompanyLogo(string domain)
         {
             HttpWebRequest wReq = (HttpWebRequest)HttpWebRequest.Create("http://logo.clearbit.com/" + domain);
+            wReq.Headers.Add("Accept-Encoding", "gzip, deflate");
             using (HttpWebResponse wResp = (HttpWebResponse)wReq.GetResponse())
             {
                 return new Bitmap(wResp.GetResponseStream());
